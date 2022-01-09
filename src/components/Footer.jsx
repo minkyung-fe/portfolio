0d6f01dec7aae97bfd46a02cbc32105ebff7feb3
전민경 <minkyung.fe@gmail.com>
2022-01-10 00:17:14 +0900

    layout 퍼블리싱

---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+function Footer() {
+  return (
+    <>
+      <footer className="footer">
+        <div className="inner">
+          <p>Copyright 2022. 전민경 All Rights Reserved.</p>
+          <p>
+            본 사이트는 상업적 목적이 아닌 개인 포트폴리오 사이트로
+            제작되었습니다.
+          </p>
+        </div>
+      </footer>
+      <div className="site-cursor"></div>
+      <div className="ie-txt">
+        <p>
+          본 사이트는 익스플로러가 지원하지 않는 기능이 많이
+          <br />
+          포함되어있습니다.크롬 브라우저로 접속 해주시면 감사하겠습니다.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+import logo from "../assets/images/logo.png";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__logo">
+        <Link to="/">
+          <img src={logo} alt="logo" />
+          <span>HOME</span>
+        </Link>
+      </h1>
+      <nav className="gnb">
+        <button className="gnb__btn">
+          <span></span>
+          <span></span>
+        </button>
+        <div className="gnb__menu">
+          <ul>
+            <li>
+              <Link to="/work">Work</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
