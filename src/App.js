3cbd94f9ad093492c1b02a29eaac75fc488690b4
전민경 <minkyung.fe@gmail.com>
2022-01-10 00:16:00 +0900

    App 정리

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "./pages/Main";
+import Work from "./pages/Work";
+import Contact from "./pages/Contact";
+import Header from "./components/Header";
+import "./assets/css/index.scss";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Header></Header>
+        <main className="container">
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/work" element={<Work />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer></Footer>
+      </BrowserRouter>
+    </>
   );
 }
 
