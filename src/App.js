b8e0567601ba13b9c6219c30c203b9a906c5db54
전민경 <minkyung.fe@gmail.com>
2022-01-11 23:14:21 +0900

    CSS연결

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./assets/css/index.scss";
 import Main from "./pages/Main";
 import Work from "./pages/Work";
 import Contact from "./pages/Contact";
 import Header from "./components/Header";
-import "./assets/css/index.scss";
 import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Header></Header>
-        <main className="container">
-          <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path="/work" element={<Work />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
-        <Footer></Footer>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Header></Header>
+      <main className="container">
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/work" element={<Work />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer></Footer>
+    </BrowserRouter>
   );
 }
 
