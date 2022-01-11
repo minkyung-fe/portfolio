f9c2a4251ea146b822d6bd10f2746f9d2a876745
전민경 <minkyung.fe@gmail.com>
2022-01-11 23:14:53 +0900

    마우스커서 추가

---
+++ src/components/Cursor.jsx
@@ -0,0 +1,13 @@
+import { useRef } from "react";
+
+const Cursor = () => {
+  const dot = useRef(null);
+  const dotOutline = useRef(null);
+
+  return (
+    <>
+      <div ref={dotOutline} className="cursor-dot-outline"></div>
+      <div ref={dot} className="cursor-dot"></div>
+    </>
+  );
+};
